3f01894ccb14cb6d88d2606ea2f8182f954de06e
Aiden Fox Ivey <aiden@fox-ivey.com>
2023-12-25 16:19:55 +0000

    Add .editorconfig and update main.py

--- main.py
+++ main.py
@@ -1,41 +1,8 @@
 #!/bin/env python3
 
-from enum import Enum
+
+from construct import Struct, Const, Int8ul, Int16ul, Array, Byte, this, Padding, Int32ul, Int64ul
 import sys
-
-class Elf:
-    pass
-
-
-class Class(Enum):
-    ThirtyTwo = 1
-    SixtyFour = 2
-
-
-class Endian(Enum):
-    Little = 1
-    Big = 2
-
-
-class Version(Enum):
-    One = 1
-
-
-class OSABI(Enum):
-    SysV = 0
-
-
-class Type(Enum):
-    NONE = 0
-    REL = 1
-    EXEC = 2
-    DYN = 3
-    CORE = 4
-
-
-class Machine(Enum):
-    X86 = 1
-    X86_64 = 2
 
 
 def main():
@@ -45,24 +12,30 @@ def main():
 
     with open(sys.argv[1], "rb") as f:
         bytes = f.read()
-        magic = bytes[0:4]
-
-        if magic != b'\x7fELF':
-            print("Not an ELF file!")
-            exit(1)
-
-        class_ = int(bytes[4])
-        endian = int(bytes[5])
-        version = int(bytes[6])
-        os_abi = int(bytes[7])
-        type = int.from_bytes(bytes[16:18], byteorder="little")
-        machine = bytes[18:20]
-        entry_point = int.from_bytes(bytes[24:32], byteorder="little")
-        tbl_offset_prg = int.from_bytes(bytes[32:40], byteorder="little")
-        tbl_offset_sec = int.from_bytes(bytes[40:48], byteorder="little")
-
-        print(f"{magic} {class_} {endian} {version} {os_abi}")
-        print(f"{type} {machine} {entry_point} {tbl_offset_prg} {tbl_offset_sec}")
+        format = Struct(
+            "signature" / Const(b"\x7fELF"),
+            "class" / Int8ul,
+            "endian" / Int8ul,
+            "version" / Int8ul,
+            "abi" / Int8ul,
+            Padding(8),
+            "type" / Int16ul,
+            "machine" / Int16ul,
+            "version" / Int32ul,
+            "entry_point" / Int64ul,
+            "program_header_offset" / Int64ul,
+            "section_header_offset" / Int64ul,
+            "flags" / Int32ul,
+            "header_size" / Int16ul,
+            "entry_size" / Int16ul,
+            "entry_count" / Int16ul,
+            "entry_index" / Int16ul,
+            "section_size" / Int16ul,
+            "section_count" / Int16ul,
+            "section_index" / Int16ul,
+        )
+        data = format.parse(bytes)
+        print(data)
 
 
 if __name__ == "__main__":
